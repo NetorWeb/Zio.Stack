f268e4cceba6391e1c5a4c7d4e4d742688ea4439
NetorWeb <1803870943@qq.com>
2023-04-18 16:54:35 +0800

    AppCollect

---
+++ Backend/src/MicroServices/AppCollect/AppCollect.Api/Middleware/ConfigurationMiddleware.cs
@@ -0,0 +1,19 @@
+﻿namespace AppCollect.Api.Middleware;
+
+public static class ConfigurationMiddleware
+{
+    public static void AddCustomConfiguration(this WebApplicationBuilder builder)
+    {
+        builder.Configuration.AddConfiguration(GetConfiguration()).Build();
+    }
+
+    static IConfiguration GetConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        return builder.Build();
+    }
+}
---
+++ Backend/src/MicroServices/AppCollect/AppCollect.Api/Program.cs
@@ -0,0 +1,32 @@
+using AppCollect.Api.Middleware;
+using Log.Serilog.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddCustomConfiguration();
+
+var log = builder.AddCustomSerilog();
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
