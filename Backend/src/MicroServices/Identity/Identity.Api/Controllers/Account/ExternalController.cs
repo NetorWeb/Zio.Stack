7d189d5fbe6f38c3e36066495adc25e7b86cde58
NetorWeb <1803870943@qq.com>
2023-04-18 15:56:39 +0800

    1

---
+++ Backend/src/MicroServices/Identity/Identity.Api/Controllers/Account/ExternalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Identity.Api.Controllers.Account;
+
+public class ExternalController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
